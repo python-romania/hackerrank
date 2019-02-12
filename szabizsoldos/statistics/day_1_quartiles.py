a8017e9f69e999eae78c1cff83da85d78c35fb1c
Szabolcs Zsoldos <zsoldos@materom.ro>
2019-02-12 09:30:31 +0200

    print() is enough after the if/else statement, code refactoring

--- szabizsoldos/statistics/day_1_quartiles.py
+++ szabizsoldos/statistics/day_1_quartiles.py
@@ -25,17 +25,11 @@ def calculate_quartiles(total_integers, integer_list_collection):
         sorted_list = sorted(integer_list_collection)
 
         if(total_integers % 2 == 1):
-
             q2 = int(calculate_median(integer_list_collection))
             q1 = int(calculate_median(integer_list_collection[0:integer_list_collection.index(q2)]))
             q3 = int(calculate_median(integer_list_collection[integer_list_collection.index(q2)+1:]))
 
-            print(q1)
-            print(q2)
-            print(q3)
-
         else:
-
             lower_half = sorted_list[0:int(len(sorted_list) / 2)]
             upper_half = sorted_list[int(len(sorted_list) / 2):]
 
@@ -43,9 +37,10 @@ def calculate_quartiles(total_integers, integer_list_collection):
             q1 = int(calculate_median(lower_half))
             q3 = int(calculate_median(upper_half))
 
-            print(q1)
-            print(q2)
-            print(q3)
+        # Print is enough after the if/else statement, code refactoring
+        print(q1)
+        print(q2)
+        print(q3)
     else:
         return None
 
